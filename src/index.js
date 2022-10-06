a782ee07659af44861fc7135c0a35cfac44cf373
iitaellu <78799868+iitaellu@users.noreply.github.com>
2022-10-06 20:05:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import L from "leaflet";
 
-let map;
-
 //Help from course's sourscodes
 const fetchData = async () => {
   const url =
@@ -9,11 +7,13 @@ const fetchData = async () => {
   const res = await fetch(url);
   const data = await res.json();
 
+  console.log(data);
+
   initMap(data);
 };
 
 const initMap = (data) => {
-  map = L.map("map", {
+  let map = L.map("map", {
     minZoom: -3
   });
 
@@ -45,20 +45,15 @@ fetchData();
     "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
   const res = await fetch(url);
   const data = await res.json();
-
   initMap(data);
 };
-
 const initMap = (data) => {
   let map = L.map("map", {
     minZoom: -3
   })
-
   let geoJson = L.geoJSON(data, {
     weight: 2
   }).addTo(map);
-
   map.fitBounds(geoJson.getBounds());
 };
-
 fetchData();*/
